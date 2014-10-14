649fc9525e88a0fef74ffa7463c2d737bbe73844
Tao Huang <ht.anglenx@gmail.com>
2014-10-14 14:23:48 +0800

    add data

--- main.rb
+++ main.rb
@@ -1,24 +1,18 @@
-# -*- coding: UTF-8 -*-
+# coding: utf-8
 
 require "erb"
 require 'csv'
 
-Class Unit
+class Unit
 	attr_accessor :one,:two,:area
-	def initialize(one,two,area)
-		@one = one
-		@two = two
-		@area = area
+
+	def to_s
+		"#{@one},#{@two},#{@area}\n"
 	end
 end
 
-Class Land
+class Land
 	attr_accessor :num,:local,:units
-	def initialize(num,local,units)
-		@num = num
-		@local = local
-		@units = units
-	end
 
 	def area
 		@units.inject(0){|sum,x| sum + x.area}
@@ -43,17 +37,15 @@ Class Land
 		gd = @units.select { |e| e.one == "建设用地" }
 		gd.inject(0){|sum,x| sum + x.area}
 	end
+
+	def to_s
+		"#{@num},#{@local[0]},#{@units}\n"
+	end
 end
 
-Class Project
+class Project
 	attr_accessor :name,:local,:lands
 	
-	def initialize(name,local,lands)
-		@name = name
-		@local = local
-		@lands = lands
-	end
-
 	def area
 		@lands.inject(0){|sum,x| sum + x.area}
 	end
@@ -61,15 +53,14 @@ Class Project
 	def length
 		@lands.length
 	end
+
+	def to_s
+		"#{@name},#{@local[0][0]},#{@lands}\n"
+	end
 end
 
-Class Region
+class Region
 	attr_accessor :name,:projects
-
-	def initialize(name,projects)
-		@name = name
-		@projects = projects
-	end
 
 	def area
 		@projects.inject(0){|sum,x| sum + x.area}
@@ -78,16 +69,14 @@ Class Region
 	def length
 		@projects.length
 	end
+
+	def to_s
+		"#{@name},#{@projects}\n"
+	end
 end
 
-Class  District
+class  District
 	attr_accessor :name,:input,:output
-
-	def initialize(name,input,output)
-		@name = name
-		@input = input
-		@output = output
-	end
 
 	def area
 		input.area + output.area
@@ -95,13 +84,8 @@ Class  District
 
 end
 
-Class Downtown
+class Downtown
 	attr_accessor :xinbei,:wujing
-
-	def initialize(xinbei,wujing)
-		@xinbei = xinbei
-		@wujing = wujing
-	end
 
 	def all_input_count
 		@xinbei.input.length + @wujing.input.length
@@ -118,49 +102,83 @@ Class Downtown
 	def all_output_area
 		@xinbei.output.area + @wujing.output.area
 	end
-
-
 end
 
 
-lines = CSV.open(ARGV[0]).readlines
+lines = CSV.open(ARGV[0],"r:utf-8").readlines
 keys = lines.delete lines.first
-erb = ERB.new( temp.read )
-output.write erb.result(binding)
 
-lines.each do |i|
-	bz = i[2]
-	dkxh = i[3]
-	tzlxdm = i[4]
-	one = i[6]
-	two = i[7]
-	xzq_q = i[8]
-	xzq_z = i[9]
-	xzq_x = i[10]
-	area = i[11].to_f
+# lines.each do |i|
+# 	bz = i[2]
+# 	dkxh = i[3]
+# 	tzlxdm = i[4]
+# 	one = i[6]
+# 	two = i[7]
+# 	xzq_q = i[8]
+# 	xzq_z = i[9]
+# 	xzq_x = i[10]
+# 	area = i[11].to_f
 
-	unit = Unit.new(one,two,area)
-end
+# 	unit = Unit.new(one,two,area)
+# end
 
-wujing = lines.select { |e| xzq_q == "武进区" }
-input = wuing.select{|e| tzlxdm == "I"}
-output = wuing.select{|e| tzlxdm == "O"}
+wujing = lines.select { |e| e[8] == "武进区" }
+puts wujing.length
+input = wujing.select{|e| e[4] == "I"}
+puts input.length
+output = wujing.select{|e| e[4] == "O"}
 
 inputPrjects = {}
 input.each do |e| 
-	name = ""
-	if e[2] == ""
-		name = e[3]
-	else
-		name = e[2]
-	end
-
+	name = e[2]==""? e[3]:e[2]
 	if inputPrjects.has_key?(name)
 		inputPrjects[name] << e
 	else
 		inputPrjects[name] = []
 	end
 end
+
+conditional = Region.new
+conditional.name = "有条件调为允许"
+conditional.projects = []
+
+inputPrjects.each do |name,units|
+	project = Project.new
+	project.name = name
+	project.local = units.uniq{|q| q[9]}
+	project.lands = []
+
+    lands = {}	
+	units.each do |e|  
+		if lands.has_key?(e[3])
+			lands[e[3]] << e
+		else
+			lands[e[3]] = []
+		end
+	end
+
+	lands.each do |key,units|
+		land = Land.new
+		land.num = key
+		land.local = units.uniq{|q| q[10]}
+		land.units = []
+
+		units.each do |e|  
+			u = Unit.new
+			u.one = e[6]
+			u.two = e[7]
+			u.area = e[11].to_f
+
+			land.units << u
+		end
+
+		project.lands << land
+	end
+
+	conditional.projects << project
+end
+
+puts conditional.projects[0]
 
 
 
